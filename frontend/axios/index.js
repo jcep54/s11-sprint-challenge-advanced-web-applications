a173e8e681b4f433e1c0099dca00c0675ab72e38
Jose Cepero <ceperojose54@gmail.com>
2023-08-10 14:50:25 -0400

    added code for axios with auth

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,14 @@
 // ✨ implement axiosWithAuth
+
+
+import axios from "axios";
+
+const axiosWithAuth = () =>{
+    const token = localStorage.getItem('token');
+    return axios.create({
+        baseURL: `http://localhost:9000/api/articles`,
+        headers: {authorization: token}
+    })
+}
+
+export default axiosWithAuth;
